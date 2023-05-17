41d9c53a93c5e7a735e979c4066d1e528867a9be
IsaiasSPinto <isaiascxs10@gmail.com>
2023-05-17 10:11:01 -0300

    read function

--- index.ts
+++ index.ts
@@ -3,10 +3,51 @@ const DB_FILE_PATH = "./db";
 
 console.log("[CRUD]");
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
-  fs.writeFileSync(DB_FILE_PATH, content);
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Todo[] = [...read(), todo];
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
   return content;
 }
 
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+
+  if (!db.todos) {
+    return [];
+  }
+
+  return db.todos;
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "{}");
+}
+
 //[Simulation]
-console.log(create("Hoje eu precisso gravar aulas!"));
+CLEAR_DB();
+create("Primeira TODO");
+create("Segunda TODO");
+console.log(read());
